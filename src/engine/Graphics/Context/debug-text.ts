e149228275966e9a12a3a9c78654ced4659676ae
Erik Onarheim <erik.onarheim@gmail.com>
2021-11-27 07:13:03 -0600

    revertme: wip optimizations (too far)

--- src/engine/Graphics/Context/debug-text.ts
+++ src/engine/Graphics/Context/debug-text.ts
@@ -1,9 +1,6 @@
-import { ExcaliburGraphicsContext, ImageSource /*, SpriteFont, SpriteSheet*/ } from '..';
+import { ExcaliburGraphicsContext, ImageSource, SpriteFont, SpriteSheet } from '..';
 import { Vector } from '../..';
 import debugFont from './debug-font.png';
-import consolasSDF from './TextRenderer/consolas-sdf.png';
-import consolasJSON from './TextRenderer/consolas-sdf.json';
-import { TextRenderer } from './TextRenderer/text-renderer';
 
 /**
  * Internal debugtext helper
@@ -18,31 +15,28 @@ export class DebugText {
    */
   public readonly fontSheet = debugFont;
   public size: number = 16;
-  // private _imageSource: ImageSource;
-  private _imageSource2: ImageSource;
-  // private _spriteSheet: SpriteSheet;
-  // private _spriteFont: SpriteFont;
+  private _imageSource: ImageSource;
+  private _spriteSheet: SpriteSheet;
+  private _spriteFont: SpriteFont;
   public load() {
-    // this._imageSource = new ImageSource(this.fontSheet);
-    this._imageSource2 = new ImageSource(consolasSDF);
-    return this._imageSource2.load();
-    // return this._imageSource.load().then(() => {
-    //   this._spriteSheet = SpriteSheet.fromImageSource({
-    //     image: this._imageSource,
-    //     grid: {
-    //       rows: 3,
-    //       columns: 16,
-    //       spriteWidth: 16,
-    //       spriteHeight: 16
-    //     }
-    //   });
-    //   this._spriteFont = new SpriteFont({
-    //     alphabet: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ,!\'&."?-()+ ',
-    //     caseInsensitive: true,
-    //     spriteSheet: this._spriteSheet,
-    //     spacing: -6
-    //   });
-    // });
+    this._imageSource = new ImageSource(this.fontSheet);
+    return this._imageSource.load().then(() => {
+      this._spriteSheet = SpriteSheet.fromImageSource({
+        image: this._imageSource,
+        grid: {
+          rows: 3,
+          columns: 16,
+          spriteWidth: 16,
+          spriteHeight: 16
+        }
+      });
+      this._spriteFont = new SpriteFont({
+        alphabet: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ,!\'&."?-()+ ',
+        caseInsensitive: true,
+        spriteSheet: this._spriteSheet,
+        spacing: -6
+      });
+    });
   }
 
   /**
@@ -52,9 +46,8 @@ export class DebugText {
    * @param pos
    */
   public write(ctx: ExcaliburGraphicsContext, text: string, pos: Vector) {
-    if (this._imageSource2.isLoaded()) {
-      ctx.draw<TextRenderer>('text', this._imageSource2.image, consolasJSON, pos, text);
-      // this._spriteFont.render(ctx, text, pos.x, pos.y);
+    if (this._imageSource.isLoaded()) {
+      this._spriteFont.render(ctx, text, pos.x, pos.y);
     }
   }
 }
